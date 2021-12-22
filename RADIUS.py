3480b5898eaa296bd56fc6449931c7e80022995e
GOKILA T <95522214+GOKILATHIRUPPATHI@users.noreply.github.com>
2021-12-22 15:31:00 +0530

    Update RADIUS.py

--- RADIUS.py
+++ RADIUS.py
@@ -3,7 +3,6 @@ radius = float(input("Input the radius of the circle: "))
 x=area = math.pi * radius ** 2
 print("The area of the circle with radius is " ,x)
 
-filename = input("Input the FileName: ")
-f_extns = filename.split(".")
-py='python'
-print ("The extension of the file is : " + repr(f_extns[-1]),repr(py))
+filename = input("Input the Filename: ")
+f = filename.split(".")
+print ("The extension of the file is : " + (f[-1]))
